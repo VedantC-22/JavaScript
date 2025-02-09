6f643943189849df40954738b562950ae0f1ef86
VedantC-22 <chaudharivedant143@gmail.com>
2025-02-10 00:09:02 +0530

    javascript updated

---
+++ c1_Basics/01_Variables.js
@@ -0,0 +1,16 @@
+const ACCOUNTID = 2283
+let email = "chaudharivedant143@gmail.com"
+var name = "Vedant Chaudhari"
+accountCity = "Pune"
+
+// ACCOUNTID = 5225
+email = "vedantchaudhari@gmail.com"
+name = "Chaudhari Vedant"
+accountCity = "Mumbai"
+
+{
+   let email = "hello@gmail.com"
+}
+
+console.table([ACCOUNTID, name, email, accountCity])
+
---
+++ c1_Basics/02_datatypes.js
@@ -0,0 +1,47 @@
+"user strict"; //treat all js code as newer version
+
+
+//number => range 2 to power 53
+//bigint
+//string => ""
+//boolean => flase/true
+//null => empty -> assigned as null
+//undefined => declared but uninitialized
+//objects
+
+let num = 22
+let name = "Vedant"
+let email;
+
+console.log(typeof(null))
+console.log(typeof(email))
+
+//----------datatype conversion-------------
+console.log("----------datatype conversion-------------")
+console.log("----------------------------------\n1.Number")
+let age = 21
+let n = "22vc"
+let number = "22"
+let valInNumber = Number(n)
+console.log(typeof age)
+console.log(typeof valInNumber)
+console.log(valInNumber)
+console.log(Number(null))
+console.log(Number(undefined))
+console.log(typeof number)
+console.log(Number(number))
+
+console.log("----------------------------------\n2.Boolean")
+//true -> 1, false -> 0
+let a = 0 //false
+a = 1 //true
+a = "" //false
+a = "A" //true
+a = null //false
+a = undefined //false
+let flag = Boolean(a)
+console.log(flag)
+
+console.log("----------------------------------\n3.String")
+let x  = 33
+console.log (typeof String(x))
