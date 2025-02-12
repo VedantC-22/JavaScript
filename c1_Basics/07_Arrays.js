14541661a1aa14da489deff1e0f004624e5da2b1
VedantC-22 <chaudharivedant143@gmail.com>
2025-02-12 12:47:24 +0530

    javascript updated

--- c1_Basics/07_Arrays.js
+++ c1_Basics/07_Arrays.js
@@ -2,6 +2,24 @@ let arr = ['a', 'b', 'v']
 console.log(arr)
 console.log(typeof arr)
 console.log(arr.reverse())
+
+//array operations
+arr.push('d') //[ 'v', 'b', 'a', 'd' ]
+arr.pop() //[ 'v', 'b', 'a' ]
+arr.unshift('e') //[ 'e', 'v', 'b', 'a' ]
+arr.shift()
+console.log(arr)
+
+//cacatination
+let arr2 = ['e', 'f', 'g','h']
+let newArray = arr.concat(arr2) //['v', 'b', 'a','e', 'f', 'g','h']
+console.log(newArray)
+// arr.push(arr2) //[ 'v', 'b', 'a', [ 'e', 'f', 'g', 'h' ] ]
+// console.log(arr)
+//using spread operator
+newArray = [...arr2, ...arr]
+console.log(newArray) //['e', 'f', 'g','h', 'v', 'b', 'a']
+
 let myArray = new Array(0,1,2,3,4,5)
 console.log("A:",myArray)
 console.log("A array length: ", myArray.length)
@@ -59,3 +77,133 @@ console.log("A: ", myArray)
 // The toSpliced() method of Array instances is the copying version of the splice() method. 
 // It returns a new array with some elements removed and/or replaced at a given index.
 // It doesn't modify the original array.
+
+
+//toLocaleString()
+console.log("---------toLocaleString()-----------")
+const prices = ["￥7", 500, 8123, 12];
+prices.toLocaleString("ja-JP", { style: "currency", currency: "JPY" });
+
+// "￥7,￥500,￥8,123,￥12"
+
+const arrayLike = {
+    length: 3,
+    0: 1,
+    1: 2,
+    2: 3,
+    3: 4, // ignored by toLocaleString() since length is 3
+  };
+  console.log(Array.prototype.toLocaleString.call(arrayLike));
+  // 1,2,3
+  
+  //toSorted() -> The toSorted() method of Array instances is the copying version of the sort() method. It returns a new array with the elements sorted in ascending order.
+  //toReverserd() -> The toReversed() method of Array instances is the copying counterpart of the reverse() method. It returns a new array with the elements in reversed order.
+
+//The copyWithin() method of Array instances shallow copies part of this array to another location in the same array and returns this array without modifying its length.
+//copyWithin(target, start)
+// copyWithin(target, start, end)
+let array1 = ["a", "b", "c", "d", "e"];
+
+// Copy to index 0 the element at index 3
+console.log(array1.copyWithin(0, 3, 4));
+// Expected output: Array ["d", "b", "c", "d", "e"]
+
+// Copy to index 1 all elements from index 3 to the end
+console.log(array1.copyWithin(1, 3));
+// Expected output: Array ["d", "d", "e", "d", "e"]
+
+// Sparse arrays
+// Arrays can contain "empty slots", which are not the same as slots filled with the value undefined.
+// Array constructor:
+const a = Array(5); // [ <5 empty items> ]
+
+// Consecutive commas in array literal:
+const b = [1, 2, , , 5]; // [ 1, 2, <2 empty items>, 5 ]
+
+// Directly setting a slot with index greater than array.length:
+const c = [1, 2];
+c[4] = 5; // [ 1, 2, <2 empty items>, 5 ]
+
+// Elongating an array by directly setting .length:
+const d = [1, 2];
+d.length = 5; // [ 1, 2, <3 empty items> ]
+
+// Deleting an element:
+const e = [1, 2, 3, 4, 5];
+delete e[2]; // [ 1, 2, <1 empty item>, 4, 5 ]
+
+console.log("-----------keys()-----------")
+// The keys() method of Array instances returns a new array iterator object that contains the keys for each index in the array.
+array1 = ["a", "b", "c"];
+const iterator = array1.keys();
+
+for (const key of iterator) {
+  console.log(key);
+}
+
+// Expected output: 0
+// Expected output: 1
+// Expected output: 2
+
+// Using keys() on sparse arrays
+// Unlike Object.keys(), which only includes keys that actually exist in the array, the keys() iterator doesn't ignore holes representing missing properties.
+arr = ["a", , "c"];
+const sparseKeys = Object.keys(arr);
+const denseKeys = [...arr.keys()];
+console.log(sparseKeys); // ['0', '2']
+console.log(denseKeys); // [0, 1, 2]
+
+console.log("-----------reduceRight()------------")
+// The reduceRight() method of Array instances applies a function against an accumulator and each value of the array (from right-to-left) to reduce it to a single value.
+array1 = [
+    [0, 1],
+    [2, 3],
+    [4, 5],
+  ];
+  
+  const result = array1.reduceRight((accumulator, currentValue) =>
+    accumulator.concat(currentValue),
+  );
+  
+  console.log(result);
+  // Expected output: Array [4, 5, 2, 3, 0, 1]
+
+//   reduce()
+// The reduce() method executes a reducer function on each element of the array and returns a single output value.
+
+// The first time that the callback is run there is no "return value of the previous calculation". 
+// If supplied, an initial value may be used in its place. 
+// Otherwise the array element at index 0 is used as the initial value and iteration starts from the next element (index 1 instead of index 0).
+const message = ["JavaScript ", "is ", "fun."];
+
+// function to join each string elements
+function joinStrings(accumulator, currentValue) {
+  return accumulator + currentValue;
+}
+
+// reduce join each element of the string
+let joinedString = message.reduce(joinStrings);
+console.log(joinedString);
+
+// Output: JavaScript is fun.
+
+console.log(Array.isArray(message)) //The isArray() method checks whether the passed argument is an array or not.
+console.log(Array.from("Vedant"))
+console.log(Array.from({name: "Vedant"}))
+// Array.from(arraylike, mapFunc, thisArg)
+// function that returns a new array
+function createArray(arraylike, mapFunc) {
+    return Array.from(arraylike, mapFunc);
+  }
+  
+  // using arrow function for mapFunc
+  result = createArray([2, 4, 6], (element) => element + 2);
+  
+  console.log(result);
+{
+    let score1 = 100
+    let score2 = 200
+    let score3 = 300
+    console.log(Array.of(score1, score2, score3)) //The of() method creates a new array instance from the given arguments.
+}
+
---
+++ c1_Basics/08_Objects.js
@@ -0,0 +1,133 @@
+//singleton
+// Object.create()
+//object literals
+
+let mySym = Symbol("Hello")
+let user = {
+    name: "Vedant",
+    "full name": "Vedant Chuadhari",
+    age: 21,
+    [mySym] : "key1",
+    location: "Pune",
+    isLoggedIn: false,
+    greet: function() { //() =>
+        console.log(`Hello ${this.name}`)
+    },
+}
+
+//accessing object values
+console.log(user.name)
+console.log(user["age"])
+// console.log(user.full name)
+console.log(user["full name"])
+console.log(user[mySym])
+//adding property
+user.email = "chaudharivedant@gmail.com"
+console.log(user)
+user.greet();
+
+console.log(user)
+user["age"] = 22
+
+console.log(Object.keys(user))
+console.log(Object.values(user))
+console.log(Object.entries(user))
+
+//hasOwnProperty(propery)
+console.log("----------hasOwnProperty()------------")
+console.log(user.hasOwnProperty('isLoggedIn'))
+
+console.log("----------Object.freeze()------------")
+// The Object.freeze() static method freezes an object. 
+// Freezing an object prevents extensions and makes existing properties non-writable and non-configurable. 
+// A frozen object can no longer be changed: new properties cannot be added, existing properties cannot be removed, 
+// their enumerability, configurability, writability, or value cannot be changed, and the object's prototype cannot be re-assigned. 
+// freeze() returns the same object that was passed in.
+Object.freeze(user)
+user["age"] = 21
+//adding element
+// silently doesn't add the property
+user.mobileno = "0000001111"
+console.log(user)
+
+// The Object.seal() static method seals an object. 
+// Sealing an object prevents extensions and makes existing properties non-configurable. 
+// A sealed object has a fixed set of properties: new properties cannot be added, existing properties cannot be removed, 
+// their enumerability and configurability cannot be changed, and its prototype cannot be re-assigned. 
+// Values of existing properties can still be changed as long as they are writable. 
+// seal() returns the same object that was passed in.
+
+
+const realUser = {
+    email: "chaudharivedant@gmail.com",
+    fullName: {
+        userFullName: {
+            firstName: "Vedant",
+            lastName: "Chaudhari"
+        }
+    },
+    age: 21
+}
+
+console.log(realUser)
+console.log(realUser.fullName.userFullName.firstName)
+
+//Merging objects
+const obj1 = {1: 'a', 2: 'b', 3:'c'}
+const obj2 = {4: 'c', 5: 'd', 6: 'e'}
+const obj3 = {obj1, obj2}
+console.log(obj1, obj2)
+
+const obj4 = {...obj1, ...obj2}
+console.log(obj4)
+
+// The Object.assign() static method copies all enumerable own properties from one or more source objects to a target object. 
+// It returns the modified target object.
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
+
+const returnedTarget = Object.assign(target, source);
+
+console.log(target);
+// Expected output: Object { a: 1, b: 4, c: 5 }
+
+console.log(returnedTarget === target);
+// Expected output: true
+const source2 = {x:25, y: 53, z:89}
+const newObj = Object.assign({},source, source2 )
+console.log(source)
+console.log(source2)
+console.log(newObj)
+
+const users = [
+    {
+        id: 1,
+        name: "Vedant"
+    },
+    {
+        id: 2,
+        name: "Chetan"
+    },
+    {
+        id: 3,
+        name: "Dhiraj"
+    }
+]
+
+console.log(users[0].name)
+
+
+//Object de-structuring
+// Destructuring Assignment is a JavaScript expression that allows to unpack of values from arrays, or properties from objects,
+// into distinct variables data can be extracted from arrays, objects, and nested objects, and assigned to variables.
+console.log("-------Object de-structuring--------")
+const course = {
+    courseName: "vc javascript learning",
+    price: 499,
+    courseInstructor: "vc"
+}
+
+// course.courseInstructor
+// const {courseInstructor, price} = course
+const {courseInstructor: instructor, price} = course
+console.log(instructor) 
